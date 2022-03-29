76f6c53ca0b8b8204f8c75a0fe48450fff9c5af2
Maycon Araujo <97076264+MayconArj@users.noreply.github.com>
2022-03-29 11:13:02 -0300

    Add files via upload

---
+++ author.py
@@ -0,0 +1,29 @@
+def author():
+
+    print("\n\n\n\n\n\n\n\033[1;37mOlá, me chamo \033[1;32mMaycon Douglas de Araujo\033[1;37m. Este programa foi desenvolvido com o objetivo oferecer suporte aos muitos fãs de Pokemon!")
+    print("\nA linguaguem usada neste programa foi \033[1;32mPython 3.8.10\033[1;37m")
+    print("\nPor enquanto, foram colocados apenas os ataques de lvl 1 de cada Pokémon nesta Pokedex.")
+    print("\nAs descrições dos pokemons foram retiradas de \033[1;32m\"Pokémon Brilliant Diamond\"\033[1;37m e \033[1;32m\"https://www.heypikachu.com\".\033[1;37m")
+    print("\nVocê pode encontrar o código deste programa acessando meu \033[1;32mportifólio: https://www.maycondouglas.com.br\033[1;33m")
+    print("\nAproveite! =)\033]m\n\n\n")
+
+    print("\033[0m\n\n\033[m")
+
+    print("\n\033[1;37mHi, my name's \033[1;32mMaycon Douglas de Araujo\033[1;37m. This program was developed with the aim of supporting the many Pokemon fans!")
+    print("\nThe language used in this program was \033[1;32mPython 3.8.10\033[1;37m")
+    print("\nFor now, only the lvl 1 attacks of each Pokémon have been placed in this Pokedex.")
+    print("\nThe pokemon descriptions were taken from \033[1;32m\"Pokémon Brilliant Diamond\"\033[1;37m and \033[1;32m\"https://www.heypikachu.com\".\033[1;37m")
+    print("\nYou can find the code for this program by accessing my \033[1;32mportfolio: https://www.maycondouglas.com.br\033[1;33m")
+    print("\nEnjoy! =)\033[m")
+
+                               
+    print("\n\033[1;36m                  .       .  .               ")
+    print("                  |-.. .  |\/|.-. . ..-.-..-.")
+    print("                  `-''-|  '  '`-`-'-|`-`-'' '")
+    print("                     `-'          `-'        \033[m")
+
+
+    
+
+
+    break_author = input("\n\n\033[1;36mAperte \"enter\" para continuar." "/" "Press \"enter\" to continue. \n\n\n\n\n\033[m")
